cafdd75bcbe54107f56fe17d42505a712ea8cc85
murphyAk <65244819+murphyAk@users.noreply.github.com>
2020-06-24 19:05:28 -0400

    requirements met

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user/add")]
+        public IActionResult SignUp(User user, string verify)
+        {
+            if (user.Password == verify)
+            {
+                ViewBag.user = user;
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.user = user;
+                ViewBag.message = "passwords don't match!";
+                return View("add");
+            }
+        }
+    }
+}
